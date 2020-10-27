519dce83a94ec04b41f56ad98a5292812dd26b0f
정원호 <wonho1401@naver.com>
2020-10-27 09:55:16 +0900

    Set up

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { Text, View } from 'react-native';
+import {Ionicons} from "@expo/vector-icons";
+import * as Font from "expo-font";
+import {Asset} from "expo-asset";
+import {AppLoading} from "expo";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [loaded, setLoaded] = useState(false);
+  const preload = async() => {
+    try {
+      await Font.loadAsync({
+        ...Ionicons.font
+      });
+      await Asset.loadAsync([require("./assets/logo.png")]);
+      setLoaded(true);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  useEffect(() => {
+    preload();
+  },[]);
+
+  return loaded ? (
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View> 
+      ): (
+      <AppLoading />
+      );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
