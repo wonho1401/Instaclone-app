407e133338c95fa576dc80b0243e202534784b01
정원호 <wonho1401@naver.com>
2020-11-05 17:34:13 +0900

    StackNavigation to TabNav (v4)

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -2,6 +2,7 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import TabNavigation from "./TabNavigation";
 import PhotoNavigation from "./PhotoNavigation";
+
 const MainNavigation = createStackNavigator(
   {
     TabNavigation,
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,19 +1,50 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import { createBottomTabNavigator } from "react-navigation-tabs";
-import { View } from "react-native";
-import Home from "../screens/Home";
-import Notifications from "../screens/Notifications";
-import Profile from "../screens/Profile";
-import Search from "../screens/Search";
+import Home from "../screens/Tabs/Home";
+import Notifications from "../screens/Tabs/Notifications";
+import Profile from "../screens/Tabs/Profile";
+import Search from "../screens/Tabs/Search";
+import { createStackNavigator } from "react-navigation-stack";
+
+const stackFactory = (initialRoute, customConfig) =>
+  createStackNavigator({
+    InitialRoute: {
+      screen: initialRoute,
+      navigationOptions: { ...customConfig },
+    },
+  });
 
 export default createBottomTabNavigator({
-  Home,
-  Search,
-  Plus: {
+  Home: {
+    screen: stackFactory(Home, {
+      title: "Home",
+      headerRight: (
+        <TouchableOpacity>
+          <Text>Msg</Text>
+        </TouchableOpacity>
+      ),
+    }),
+  },
+  Search: {
+    screen: stackFactory(Search, {
+      title: "Search",
+    }),
+  },
+  Add: {
     screen: View,
     navigationOptions: {
       tabBarOnPress: ({ navigation }) => navigation.navigate("PhotoNavigation"),
     },
   },
-  Notifications,
-  Profile,
+  Notifications: {
+    screen: stackFactory(Notifications, {
+      title: "Notifications",
+    }),
+  },
+  Profile: {
+    screen: stackFactory(Profile, {
+      title: "Profile",
+    }),
+  },
 });
